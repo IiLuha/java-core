594169fd0d7e075e75ca35060c3e014ab563eb79
IiLuha <elyamin@bk.ru>
2025-08-19 18:47:05 +0300

    Добавил скрипты по созданию и заполнению таблиц для финального задания. Добавил триггер и хранимую функцию

---
+++ src/main/resources/sql/finaltask/schema.sql
@@ -0,0 +1,53 @@
+BEGIN TRANSACTION;
+
+DROP TABLE orders;
+DROP TABLE customers;
+DROP TABLE products;
+
+COMMIT;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS orders (
+order_id SERIAL PRIMARY KEY,
+customer_id INT,
+product_id INT,
+created_at TIMESTAMP NOT NULL,
+amount NUMERIC(10,2),
+status TEXT
+);
+
+CREATE TABLE IF NOT EXISTS customers (
+customer_id SERIAL PRIMARY KEY,
+name TEXT,
+email TEXT
+);
+
+CREATE TABLE IF NOT EXISTS products (
+product_id SERIAL PRIMARY KEY,
+name TEXT,
+price NUMERIC(10,2)
+);
+
+COMMIT;
+
+BEGIN TRANSACTION;
+
+CREATE OR REPLACE FUNCTION orders_add_amount() RETURNS TRIGGER
+LANGUAGE plpgsql AS
+$$
+BEGIN
+UPDATE public.orders o
+	SET  amount=(SELECT price FROM products p WHERE new.product_id = p.product_id)
+	WHERE new.order_id = o.order_id;
+RETURN NULL;
+END
+$$;
+
+CREATE OR REPLACE TRIGGER orders_add_amount_trigger
+AFTER INSERT
+ON orders
+FOR ROW
+EXECUTE FUNCTION orders_add_amount();
+
+COMMIT;
