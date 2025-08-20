7846b336a0685fd45b7ab26eea2c332a76f601b0
IiLuha <elyamin@bk.ru>
2025-08-20 12:05:42 +0300

    Добавил неоптимизированные "тяжёлые запросы" запросы и их результаты

---
+++ src/main/resources/sql/finaltask/queries.sql
@@ -0,0 +1,59 @@
+
+--Запрос для проверки джоинов
+--Итоги:
+--Выполнено два хэш джоина и 3 сиквенс скан по всем таблицам.
+--Цена 1265к, время 50 с
+BEGIN TRANSACTION;
+
+EXPLAIN ANALYZE
+SELECT o.order_id,
+       u.name,
+       p.name,
+       o.amount,
+       p.price,
+       o.status
+FROM orders AS o
+JOIN customers AS u ON o.customer_id = u.customer_id
+JOIN products AS p ON o.product_id = p.product_id
+WHERE o.created_at BETWEEN '2025-01-01' AND '2025-01-31';
+
+COMMIT;
+
+--Запрос для проверки джоинов в другом порядке
+--То же самое, что и в предыдущем запросе. (82 с)
+BEGIN TRANSACTION;
+
+EXPLAIN ANALYZE
+SELECT o.order_id,
+       u.name,
+       p.name,
+       o.amount,
+       p.price,
+       o.status
+FROM customers AS u
+JOIN orders AS o ON o.customer_id = u.customer_id
+JOIN products AS p ON o.product_id = p.product_id
+WHERE o.created_at BETWEEN '2025-01-01' AND '2025-01-31';
+
+COMMIT;
+
+--Запрос для проверки партиционирования
+--Итоги:
+--66 с, цена 1542к, сиквенс скан
+BEGIN TRANSACTION;
+
+EXPLAIN ANALYZE
+SELECT * FROM orders WHERE created_at >= '2024-09-01' AND created_at
+<'2025-01-31';
+
+COMMIT;
+
+--запрос для ознакомления с хэш-индексом
+--Итоги:
+--85 с, цена 1057к, сиквенс скан
+BEGIN TRANSACTION;
+
+EXPLAIN ANALYZE
+SELECT * FROM orders WHERE customer_id = 32768;
+
+COMMIT;
